7295b7c94d6e986186b937f44271cdddf78c7843
Никита Продиус <nikster.nikster@gmail.com>
2020-04-17 21:15:53 +0700

    refactoring t-forgot-pass

--- src/components/t-sign/t-forgot-pass/t-forgot-pass-item/t-forgot-pass-item.vue
+++ src/components/t-sign/t-forgot-pass/t-forgot-pass-item/t-forgot-pass-item.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="t-forgot-pass-item">
         <h2 class="pl-3 main-item-head mb-4">Забыли пароль?</h2>
+        <p class="t-forgot-pass-item__text">Введите адрес электронной почты</p>
         <form action="" method="POST" class="needs-validation" v-if="showForm">
             <div class="form-group col-12">
                 <label for="email">Email</label>
@@ -16,9 +17,9 @@
         </div>
         <div class="row justify-content-center mx-0 link">
             <div class="col-12 text-center">
-                <hr class="mt-4">
+                <hr class="t-forgot-pass-item__hr">
                 <router-link :to="{name: 'login'}">
-                    <p class="">Вернуться на страницу входа</p>
+                    <p class="t-forgot-pass-item__link">Вернуться на страницу<br> входа</p>
                 </router-link>
             </div>
         </div>
@@ -49,5 +50,18 @@
 
 
 <style lang="sass">
-
+    .t-forgot-pass-item
+        &__text
+            font-size: 15px;
+            padding-left: 15px;
+        &__hr
+            border: 1px solid #C4C4C4;
+        .link
+            margin-bottom: 15px;
+        .needs-validation
+            .form-group
+                &:nth-child(1)
+                    margin-bottom: 38px;
+                &:nth-child(2)
+                    margin-bottom: 15px;
 </style>
--- src/components/t-sign/t-forgot-pass/t-forgot-pass.vue
+++ src/components/t-sign/t-forgot-pass/t-forgot-pass.vue
@@ -2,7 +2,6 @@
     <div class="t-forgot-pass containet">
         <div class="row justify-content-center">
             <div class="col-12 col-md-8 col-lg-6 col-xl-6 shadow sign">
-                <h2 class="head-todo-list mt-5 mb-4">ToDoList</h2>
                 <tForgotPass/>
             </div>
         </div>
