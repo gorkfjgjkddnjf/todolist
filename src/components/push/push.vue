c9ed2e7f2f54827aa88d7699d2e36f8ada5876d5
John Doe <zkochurin@mail.ru>
2020-04-19 01:02:19 +0700

    push

--- src/components/push/push.vue
+++ src/components/push/push.vue
@@ -1,13 +1,13 @@
 <template>
 
 
-        <div class="push text-center">
-            <transition name="animate" class="">
-                <div class="push__content p-3">
+    <transition name="" class="">
+        <div class=" text-center">
+                <div class="content p-3">
                     <slot></slot>
                 </div>
-            </transition>
         </div>
+    </transition>
 
 
 </template>
@@ -24,19 +24,51 @@ export default {
     data(){
         return {}
     },
-    computed: {}
+    computed: {},
+    methods:{
+        hidePush(){
+            //let vm = this
+            setTimeout(function () {
+
+            }),3000
+        }
+    }
 }
 </script>
 
 
-<style lang="sass">
+<style lang="sass" scoped>
 
-.push
-    background: #ffffff
-    border-radius: 10px
-    position: fixed
-    right: 3%
-    top: 6%
-    z-index: 1000
+// .push
+//     background: #ffffff
+//     border-radius: 10px
+//     position: fixed
+//     right: 3%
+//     top: 6%
+//     z-index: 1000
 
+// .animate-enter-active 
+//     transition: all .9s ease
+
+// .animate-leave-active 
+//     transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+
+// .animate-enter, .animate-leave-to
+//     transform: translateX(10px)
+//     opacity: 0
+
+// .animate-enter
+//     transform: translateX(100px)
+//     opacity: 0
+// .animate-enter-active
+//     transition: all .9s ease
+// .animate-enter-to
+//     opacity: 1
+// .animate-leave
+//     opacity: 1
+// .animate-leave-active
+//     transition: opacity .9s
+// .animate-leave-to
+//     transform: translateX(100px)
+//     opacity: 0
 </style>
